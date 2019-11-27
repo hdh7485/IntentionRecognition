92bfad66d8ca71192a3ddb722dfb6fa9dfc40eae
BoyanYK <boyan.y.kostadinov@gmail.com>
2019-11-27 16:17:46 +0900

    Generator resting vs all, skip N frames

--- generator.py
+++ generator.py
@@ -3,16 +3,17 @@ import os
 import numpy as np
 
 label_map = {
-    0: np.array([0, 0, 0, 0, 0, 0, 0, 0, 1]), #[0, 0, 0, 0],
-    1: np.array([0, 0, 0, 0, 0, 0, 0, 1, 0]), #[0, 0, 0, 1],
-    10: np.array([0, 0, 0, 0, 0, 0, 1, 0, 0]), #[0, 0, 1, 0],
-    100: np.array([0, 0, 0, 0, 0, 1, 0, 0, 0]), #[0, 1, 0, 0],
-    101: np.array([0, 0, 0, 0, 1, 0, 0, 0, 0]), #[0, 1, 0, 1],
-    110: np.array([0, 0, 0, 1, 0, 0, 0, 0, 0]), #[0, 1, 1, 0],
-    1000: np.array([0, 0, 1, 0, 0, 0, 0, 0, 0]), #[1, 0, 0, 0],
-    1001: np.array([0, 1, 0, 0, 0, 0, 0, 0, 0]), #[1, 0, 0, 1],
-    1010: np.array([1, 0, 0, 0, 0, 0, 0, 0, 0]), #[1, 0, 1, 0]
+    0: np.array([0, 0, 0, 0, 0, 0, 0, 0, 1]),  # [0, 0, 0, 0],
+    1: np.array([0, 0, 0, 0, 0, 0, 0, 1, 0]),  # [0, 0, 0, 1],
+    10: np.array([0, 0, 0, 0, 0, 0, 1, 0, 0]),  # [0, 0, 1, 0],
+    100: np.array([0, 0, 0, 0, 0, 1, 0, 0, 0]),  # [0, 1, 0, 0],
+    101: np.array([0, 0, 0, 0, 1, 0, 0, 0, 0]),  # [0, 1, 0, 1],
+    110: np.array([0, 0, 0, 1, 0, 0, 0, 0, 0]),  # [0, 1, 1, 0],
+    1000: np.array([0, 0, 1, 0, 0, 0, 0, 0, 0]),  # [1, 0, 0, 0],
+    1001: np.array([0, 1, 0, 0, 0, 0, 0, 0, 0]),  # [1, 0, 0, 1],
+    1010: np.array([1, 0, 0, 0, 0, 0, 0, 0, 0]),  # [1, 0, 1, 0]
 }
+
 
 def read_labels(file):
     with open(file) as f:
@@ -23,27 +24,33 @@ def read_labels(file):
         labels[i] = line
     return labels
 
-def generate_inputs(folder, labels_file, sequence_size, IMG_HEIGHT, IMG_WIDTH):
+
+def generate_inputs(folder, labels_file, sequence_size, IMG_HEIGHT, IMG_WIDTH, non_resting=False, skip_frames=1):
     """Create input generators for given parameters"""
-    # TODO Needs to handle resting/non-resting and every Nth frame
     # Get labels
     labels = read_labels(labels_file)
     # Enumerate files in directory
     available_ids = range(len(os.listdir(folder)))
     while True:
         # From first item to last possible (taking sequence length into consideration)
-        for i in available_ids[:-sequence_size]:
+        for i in available_ids[:- (sequence_size-1) * skip_frames]:
             sequence = []
             # Read in image and add it to list for the current sequence
             for j in range(sequence_size):
-                path = '{}/{}.jpg'.format(folder, i+j)
+                path = '{}/{}.jpg'.format(folder, i + x * j)
                 image = cv2.imread(path)
                 res = cv2.resize(image, (IMG_HEIGHT, IMG_WIDTH))
                 sequence.append(res)
+            val = int(labels[i + x * j])
             # Get label and return sequence with corresponding label
-            val = int(labels[i+j])
-            label = label_map[val]
-            yield (np.array([sequence]), np.array([label]))
+            if non_resting:
+                if val != 0:
+                    label = dic[val]
+                    yield (np.array([sequence]), np.array([label]))
+            else:
+                label = label_map[val]
+                yield (np.array([sequence]), np.array([label]))
+
 
 def chain_generators(generators):
     """Concatenate multiple generators together. From: https://stackoverflow.com/a/47592164"""
